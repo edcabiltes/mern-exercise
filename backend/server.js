6f0ad050b224d9836d07394b1ef2f67f91564842
Ed Cabiltes <edcabiltes@outlook.com>
2025-02-24 16:18:42 +0800

    connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require(`dotenv`).config();
 
 const express = require(`express`);
+const mongoose = require(`mongoose`);
 const workoutsRouter = require(`./routes/workouts`);
 
 const app = express();
@@ -13,6 +14,11 @@ app.use((req, res, next) => {
 
 app.use(`/api/workouts`, workoutsRouter);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Connected to database and running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => console.error(error));
+
